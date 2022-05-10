a8e307c230de0aebfa67e3246e9b1db202d9e4cc
George Mao <zhongqilin@gmail.com>
2022-05-10 17:05:41 -0400

    Create templates for functions

---
+++ scripts.js
@@ -0,0 +1,65 @@
+let currentOperation = [];
+let currentSum = 0;
+
+function add(a, b) {
+	return a + b;
+};
+
+function subtract(a, b) {
+	return a - b;
+};
+
+function multiply(a, b) {
+	return a * b;
+};
+
+function divide(a, b) {
+	return a / b;
+};
+
+const checkValidOperation = function(currentOperation) {
+	// check for three items in list
+	// checkOperationLength();
+
+	// check that first and third are numbers and second is op
+
+	// if (checkOperationLength && validOperation) {
+	// 	return true
+	// }
+	// else {
+	// 	return false
+	// }
+};
+
+function checkOperationLength() {
+	// if (currentOperation.length == 3) {
+	// 	return true
+	// }
+	// else {
+	// 	return false
+	// }
+}
+
+function operate() {
+	// if (checkValidOperation()) {
+	// 	parseOperation()
+	// 	calculate()	
+	// 	return `${currentSum}`;
+	// }
+	// else {
+	// 	return "error";
+	// }
+}
+
+function clearOperation() {
+	// return [];
+}
+
+function removeFromOperation() {
+	// currentOperation.pop
+}
+
+function addToOperation() {
+	// currentOperation.append();
+}
+
